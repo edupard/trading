e10f59721bcac1685c51383243cc19059b705c09
Evgeny Tarasov <tarasov.e.a@gmail.com>
2018-12-11 16:45:25 -0500

    Model averaging

--- simple_network.py
+++ simple_network.py
@@ -3,12 +3,14 @@ import tensorflow.contrib.rnn as rnn
 import numpy as np
 import copy
 
+
 class RnnNetConfig(object):
     FEATURES_DIM = 1
     LSTM_LAYERS = 3
     LSTM_LAYER_SIZE = 30
     FC_LAYERS = [100]
     DROPOUT_KEEP_RATE = 1.0
+
 
 _config_proto = RnnNetConfig()
 
@@ -18,97 +20,124 @@ def defaultRnnNetConfig():
 
 
 class RnnNet(object):
-    def __init__(self, config: RnnNetConfig):
+
+    def __init__(self, config: RnnNetConfig, graph: tf.Graph):
         self.config = config
-        print('creating neural network...')
-        with tf.Graph().as_default() as graph:
-            self.seq_len = seq_len = tf.placeholder(tf.int32, [None], name='seq_len')
-            self.labels = labels = tf.placeholder(tf.float32, [None, None], name='labels')
-            self.mask = mask = tf.placeholder(tf.float32, [None, None], name='mask')
-            self.input = input = tf.placeholder(tf.float32, [None, None, self.config.FEATURES_DIM], name='input')
-            self.keep_prob = keep_prob = tf.placeholder(tf.float32)
+        self.graph = graph
+        print('Creating model averaging network input...')
+        with graph.as_default():
+            with tf.variable_scope('input') as scope:
+                self.input = tf.placeholder(tf.float32, [None, None, self.config.FEATURES_DIM], name='input')
+                self.seq_len = tf.placeholder(tf.int32, [None], name='seq_len')
+                self.labels = tf.placeholder(tf.float32, [None, None], name='labels')
+                self.mask = tf.placeholder(tf.float32, [None, None], name='mask')
+                self.keep_prob = tf.placeholder(tf.float32)
+        self.state = {}
+        self.new_state = {}
+        self.rnn_cell = {}
+        self.returns = {}
+        self.sse = {}
+        self.cost = {}
+        self.optimizer = {}
+        self.vars = {}
+        self.grads_and_vars = {}
+        self.saver = {}
+        self.train = {}
 
-            self.rnn_cell = rnn_cell = rnn.MultiRNNCell(
-                [
-                    rnn.DropoutWrapper(rnn.LSTMCell(self.config.LSTM_LAYER_SIZE), input_keep_prob=keep_prob)
-                    for _ in range(self.config.LSTM_LAYERS)
-                ])
+    def build_wp(self, id):
+        print('creating %s weak predictor network...' % id)
+        with self.graph.as_default() as graph:
+            with tf.variable_scope(id) as scope:
 
-            state = ()
-            for s in rnn_cell.state_size:
-                c = tf.placeholder(tf.float32, [None, s.c])
-                h = tf.placeholder(tf.float32, [None, s.h])
-                state += (tf.contrib.rnn.LSTMStateTuple(c, h),)
-            self.state = state
+                self.rnn_cell[id] = rnn_cell = rnn.MultiRNNCell(
+                    [
+                        rnn.DropoutWrapper(rnn.LSTMCell(self.config.LSTM_LAYER_SIZE), input_keep_prob=self.keep_prob)
+                        for _ in range(self.config.LSTM_LAYERS)
+                    ])
 
-            # Batch size x time steps x features.
-            output, new_state = tf.nn.dynamic_rnn(rnn_cell, input, initial_state=state, sequence_length=seq_len)
-            self.new_state = new_state
+                state = ()
+                for s in rnn_cell.state_size:
+                    c = tf.placeholder(tf.float32, [None, s.c])
+                    h = tf.placeholder(tf.float32, [None, s.h])
+                    state += (tf.contrib.rnn.LSTMStateTuple(c, h),)
+                self.state[id] = state
 
-            fc_layer_idx = 0
-            for num_units in self.config.FC_LAYERS:
-                scope_name = 'fc_layer_%d' % fc_layer_idx
-                with tf.name_scope(scope_name):
-                    output = tf.contrib.layers.fully_connected(output, num_units, activation_fn=tf.nn.relu,
-                                                               scope='dense_%d' % fc_layer_idx)
-                    output = tf.nn.dropout(output, keep_prob)
-                fc_layer_idx += 1
+                # Batch size x time steps x features.
+                output, new_state = tf.nn.dynamic_rnn(rnn_cell, self.input, initial_state=state, sequence_length=self.seq_len)
+                self.new_state[id] = new_state
 
-            # final layer to make prediction
-            with tf.name_scope('prediction_layer'):
-                self.returns = tf.contrib.layers.fully_connected(output, 1, activation_fn=None)
+                fc_layer_idx = 0
+                for num_units in self.config.FC_LAYERS:
+                    scope_name = 'fc_layer_%d' % fc_layer_idx
+                    with tf.name_scope(scope_name):
+                        output = tf.contrib.layers.fully_connected(output, num_units, activation_fn=tf.nn.relu,
+                                                                   scope='dense_%d' % fc_layer_idx)
+                        output = tf.nn.dropout(output, self.keep_prob)
+                    fc_layer_idx += 1
 
-            with tf.name_scope('loss'):
-                diff = self.returns - tf.expand_dims(labels, 2)
-                self.sse = sse = tf.reduce_sum(tf.multiply(tf.square(diff), tf.expand_dims(mask, 2)))
-                self.cost = sse / tf.reduce_sum(mask)
-                self.optimizer = tf.train.AdamOptimizer()
-                self.vars = tf.trainable_variables()
-                self.grads_and_vars = self.optimizer.compute_gradients(self.cost, var_list=self.vars)
-                self.train = self.optimizer.apply_gradients(self.grads_and_vars)
+                # final layer to make prediction
+                with tf.name_scope('prediction_layer'):
+                    self.returns[id] = returns = tf.contrib.layers.fully_connected(output, 1, activation_fn=None)
 
-            self.init = tf.global_variables_initializer()
-            self.saver = tf.train.Saver(tf.global_variables(), max_to_keep=None)
-        self.sess = tf.Session(graph=graph)
+                with tf.name_scope('loss'):
+                    diff = returns - tf.expand_dims(self.labels, 2)
+                    self.sse[id] = sse = tf.reduce_sum(tf.multiply(tf.square(diff), tf.expand_dims(self.mask, 2)))
+                    self.cost[id] = cost = sse / tf.reduce_sum(self.mask)
 
-    def zero_state(self, batch_size):
+                    self.optimizer[id] = optimizer = tf.train.AdamOptimizer()
+                    self.vars[id] = vars = tf.trainable_variables(scope.name)
+                    self.grads_and_vars[id] = grads_and_vars = optimizer.compute_gradients(cost, var_list=vars)
+                    self.train[id] = optimizer.apply_gradients(grads_and_vars)
+
+                self.saver[id] = tf.train.Saver(tf.trainable_variables(scope.name), max_to_keep=None)
+
+    def init_weights(self, sess: tf.Session):
+        print('initializing weights...')
+        with self.graph.as_default() as graph:
+            init = tf.global_variables_initializer()
+            sess.run(init)
+
+    def zero_wp_state(self, id, batch_size):
         zero_state = ()
-        for s in self.rnn_cell.state_size:
+        for s in self.rnn_cell[id].state_size:
             c = np.zeros((batch_size, s.c))
             h = np.zeros((batch_size, s.h))
             zero_state += (tf.contrib.rnn.LSTMStateTuple(c, h),)
         return zero_state
 
-    def _fill_feed_dict(self, feed_dict, state):
+    def _fill_feed_dict(self, id, feed_dict, state):
         idx = 0
         for s in state:
-            feed_dict[self.state[idx].c] = s.c
-            feed_dict[self.state[idx].h] = s.h
+            feed_dict[self.state[id][idx].c] = s.c
+            feed_dict[self.state[id][idx].h] = s.h
             idx += 1
 
-    def eval(self, state, input, labels, mask, seq_len):
+    def eval_wp(self, sess, id, state, input, labels, mask, seq_len):
         feed_dict = {self.input: input, self.labels: labels, self.mask: mask, self.keep_prob: 1.0,
                      self.seq_len: seq_len}
-        self._fill_feed_dict(feed_dict, state)
+        self._fill_feed_dict(id, feed_dict, state)
 
-        new_state, sse, returns = self.sess.run((self.new_state, self.sse, self.returns), feed_dict)
+        new_state, sse, returns = sess.run((self.new_state[id], self.sse[id], self.returns[id]), feed_dict)
         return new_state, sse, returns
 
-    def fit(self, state, input, labels, mask, seq_len):
+    def fit_wp(self, sess, id, state, input, labels, mask, seq_len):
         feed_dict = {self.input: input, self.labels: labels, self.mask: mask,
                      self.keep_prob: self.config.DROPOUT_KEEP_RATE, self.seq_len: seq_len}
-        self._fill_feed_dict(feed_dict, state)
-        new_state, sse, returns, _ = self.sess.run((self.new_state, self.sse, self.returns, self.train), feed_dict)
+        self._fill_feed_dict(id, feed_dict, state)
+        new_state, sse, returns, _ = sess.run((self.new_state[id], self.sse[id], self.returns[id], self.train[id]), feed_dict)
         return new_state, sse, returns
 
-    def save_weights(self, epoch):
-        print('saving %d epoch weights' % epoch)
-        self.saver.save(self.sess, './weights', global_step=epoch, write_meta_graph=False)
+    def save_wp(self, sess, id, epoch):
+        print('saving %s %d epoch weights' % (id, epoch))
+        self.saver[id].save(sess, './weights/%s' % id, global_step=epoch, write_meta_graph=False)
 
-    def init_weights(self):
-        print('initializing weights...')
-        self.sess.run(self.init)
-
-    def load_weights(self, epoch):
+    def load_wp(self, sess, id, epoch):
         print('loading %d epoch weights' % epoch)
-        self.saver.restore(self.sess, "%s-%d" % ('./weights', epoch))
+        self.saver[id].restore(sess, "./weights/%s-%d" % (id, epoch))
+
+    def save_graph(self, graph):
+        print('saving computation graph')
+        tf.summary.FileWriter('./graph',
+                              graph)
+
+
--- train.py
+++ train.py
@@ -1,34 +1,50 @@
 from simple_network import RnnNet, defaultRnnNetConfig
 import numpy as np
+import tensorflow as tf
 
 BS = 3
 TS = 100
 F = 2
-EPOCHS = 1000
+EPOCHS = 100
 
 config = defaultRnnNetConfig()
 config.FEATURES_DIM = F
-net = RnnNet(config)
+config.DROPOUT_KEEP_RATE = 1.0
 
-net.init_weights()
+graph = tf.Graph()
+sess = tf.Session(graph=graph)
 
-for epoch in range(EPOCHS):
+net = RnnNet(config, graph)
+
+wps = ["a"]
+
+for wp in wps:
+    net.build_wp(wp)
+
+net.init_weights(sess)
+
+
+def generate_random_input():
     input = np.random.randint(0, 100, (BS, TS, F))
     labels = np.mean(input, 2)
     mask = np.ones((BS, TS), np.float32)
     seq_len = np.full((BS,), TS).reshape((-1,))
-
-    state = net.zero_state(BS)
-    state, sse, returns = net.fit(state, input, labels, mask, seq_len)
-    print(sse)
+    return input, labels, mask, seq_len
 
 
-input = np.random.randint(0, 100, (BS, TS, F))
-labels = np.mean(input, 2)
-mask = np.ones((BS, TS), np.float32)
-seq_len = np.full((BS,), TS).reshape((-1,))
+# for wp in wps:
+#     for epoch in range(EPOCHS):
+#         input, labels, mask, seq_len = generate_random_input()
+#         state = net.zero_wp_state(wp, BS)
+#         state, sse, returns = net.fit_wp(sess, wp, state, input, labels, mask, seq_len)
+#         print("%s %d %.2f" % (wp, epoch, sse))
+#     net.save_wp(sess, wp, EPOCHS)
+#
+# net.save_graph(graph)
 
-state = net.zero_state(BS)
-new_state, sse, returns = net.eval(state, input, labels, mask, seq_len)
-
-i = 0
+for wp in wps:
+    net.load_wp(sess, wp, EPOCHS)
+    input, labels, mask, seq_len = generate_random_input()
+    state = net.zero_wp_state(wp, BS)
+    new_state, sse, returns = net.eval_wp(sess, wp, state, input, labels, mask, seq_len)
+    print("Eval %s %.2f" % (wp, sse))
